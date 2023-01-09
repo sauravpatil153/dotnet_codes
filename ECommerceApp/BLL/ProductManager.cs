73904e56a8241c325da6042cc7a1836f815fc057
sauravpatil153 <sauravpatil153@gmail.com>
2023-01-09 15:25:20 +0530

    MVC app using Layered Architecture

---
+++ ECommerceApp/BLL/ProductManager.cs
@@ -0,0 +1,10 @@
+﻿namespace BLL;
+using BOL;
+using DAL;
+public class ProductManager
+{
+    public List<Product> GetAllProducts(){
+        List<Product> allProducts = DBManager.GetAllProducts();
+        return allProducts;
+    }
+}
